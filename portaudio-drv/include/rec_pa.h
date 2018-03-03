bf3b96e20a9b664127142f30b643976062c3fd41
Cassio Batista <cassio.batista.13@gmail.com>
2018-03-02 21:10:20 -0300

    pa finally working; callback issues fixed with a fake 'fifo'

--- portaudio-drv/include/rec_pa.h
+++ portaudio-drv/include/rec_pa.h
@@ -8,8 +8,8 @@
 #include <portaudio.h>
 
 #define PA_SAMPLE_RATE        16000
-#define PA_FRAMES_PER_BUFFER  1024
-#define NUM_SECONDS           1
+#define PA_FRAMES_PER_BUFFER  2048
+#define NUM_SECONDS           10
 #define PA_NUM_CHANNELS       1    /* mono=1, stereo=2 */
 
 /* #define DITHER_FLAG    paDitherOff */
--- portaudio-drv/main.c
+++ portaudio-drv/main.c
@@ -2,6 +2,9 @@
 #include "rec_pa.h"
 #include "mouse_x11.h"
 
+#define C_FG_R   "\x1b[31m" /* red */
+#define C_RESET  "\x1b[0m"
+
 static volatile int keep_running = 1;
 
 void
@@ -18,7 +21,7 @@ main(void)
 	paTestData data;
 
 	long i;
-	unsigned long remainder;
+	unsigned long fifo[3];
 
 	/* handling control c */
 	signal(SIGINT, sigint_handler);
@@ -27,31 +30,40 @@ main(void)
 		return Pa_Destroy(err, &data, "create pa structures");
 	}
 
-	remainder = data.maxFrameIndex - data.frameIndex;
-	if(remainder < framesPerBuffer) {
-		finished = paComplete;
-	} else {
-		remainder = framesPerBuffer;
-	}
+	for(i=0; i<3; i++)
+		fifo[i] = -1;
+
+	i = 2;
+	while(keep_running && (err = Pa_IsStreamActive(stream)) == 1 && i > -1) {
+		if(data.frameIndex != fifo[i])
+			fifo[i--] = data.frameIndex;
+
+		Pa_Sleep(150); /* ms */
+	} /* close while */
 
 	/* TODO apply main routine here */
 	while(keep_running && (err = Pa_IsStreamActive(stream)) == 1) {
-		if(data.frameIndex > 0) {
-		for(i=0; i<10; i++)
-			printf("%d, ", data.recordedSamples[10]);
-		printf("\n");
-		}
-		Pa_Sleep(250); /* ms */
-	}
 
-	printf("%d\n", data.recordedSamples[10]);
+		if(data.frameIndex != fifo[0]) {
+			fifo[2] = fifo[1];
+			fifo[1] = fifo[0];
+			fifo[0] = data.frameIndex;
+		}
+
+		for(i=fifo[2]; i<fifo[1]; i++) {
+			fprintf(stdout, "%+04d\n", data.recordedSamples[i]);
+			fflush(stdout);
+		}
+
+		Pa_Sleep(500); /* ms */
+	} /* close while */
 
 	if(err < 0)
 		return Pa_Destroy(err, &data, "check whether stream is active");
 
 	if(!keep_running) {
-		fprintf(stdout, "Aborted\n");
-		fflush(stdout);
+		fprintf(stderr, "Aborted\n");
+		fflush(stderr);
 		err = Pa_AbortStream(stream);
 		if(err != paNoError)
 			return Pa_Destroy(err, &data, "abort stream");
