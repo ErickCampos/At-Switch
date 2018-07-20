953018b97f3fe02a8ca45ad7665b1605e4fbcd1d
Cassio Batista <cassio.batista.13@gmail.com>
2018-07-20 12:16:01 -0300

    plot update

--- portaudio-drv/results/myplot.m
+++ portaudio-drv/results/myplot.m
@@ -1,24 +1,51 @@
+clear all;
+hold off;
+close all;
+
 load out;
 
 x = abs(out);
 
 figure;
-plot(x); grid on; hold on;
-set(gca, 'XTick', 0:250:7000);
-set(gca, 'YTick', 0:1000:20000);
+a = plot(x, 'b-', 'LineWidth', 2);
+hold on;
+%grid on;
+set(gca, 'XTick', 0:250:7000  , 'FontSize', 16);
+set(gca, 'YTick', 0:1000:20000, 'FontSize', 16);
+
+plot(ones(1,length(0:5500))*1000, 'k--');
+xlim([0 5500]);
 
 chunks = [500 1000 1250 1375 1437];
 for i = chunks
-	stem(i, x(i), 'r--', 'markerfacecolor', 'r', 'linewidth', 2);
+	b = stem(i, x(i), 'ro--', 'markersize', 8, 'markerfacecolor', 'r', 'linewidth', 2);
 end
 
-chunks = [500 1000 1250 1375 1437]+1000;
+chunks = [500 1000 1250 1375 1437]+1500;
 for i = chunks
-	stem(i, x(i), 'k--', 'markerfacecolor', 'k', 'linewidth', 2);
+	c = stem(i, x(i), 'ks--', 'markersize', 10, 'markerfacecolor', 'k', 'linewidth', 2);
+	if x(i) < 1000
+		chunks = [500 1000 1250 1375 1437]+500+i;
+		for j = chunks
+			d = stem(j, x(j), 'md--',  'markersize', 10,'markerfacecolor', 'm', 'linewidth', 2);
+		end
+		break;
+	end
 end
+
+l = legend([b c d],...
+	'Teste 1: sucesso, sopro',...
+	'Teste 2: falha, não-sopro',...
+	'Teste 3: recuperação do teste 2');
+set(l, 'FontSize', 18);
+
+xlabel('Amostras',  'FontSize', 24);
+ylabel('Amplitude', 'FontSize', 24);
+
+return; % ----------------------------------------------------------------------
 
 figure;
-plot(x); grid on; hold on;
+plot(x, 'LineWidth', 2); grid on; hold on;
 set(gca, 'XTick', 0:100:7000);
 set(gca, 'YTick', 0:1000:20000);
 
@@ -32,3 +59,6 @@ for i = chunks
 	stem(i, x(i), 'k--', 'markerfacecolor', 'k', 'linewidth', 2);
 end
 xlim([300 2800]);
+
+xlabel('Amostras',  'FontSize', 20);
+ylabel('Amplitude', 'FontSize', 20);
